831885b504e0bc9e2eee5390b107819771120904
Ahmetfa37 <ahmetfarukakalin@gmail.com>
2025-02-01 11:05:43 +0300

    Add files via upload

---
+++ ödev 3 revize/script.js
@@ -0,0 +1,28 @@
+fetch("https://fakestoreapi.com/products")
+    .then(sonuc => sonuc.json())
+    .then(veri => { fonk (veri)});
+    function fonk(params) {
+        var x = document.getElementById("kart");
+        params.forEach((item) => {
+            x.innerHTML += 
+            `<div  class="kart2">
+                <img class="resim" src="${item.image} alt="${item.title}" >
+                <h5>${item.title}</h5>
+                <p>$${item.price}</p>
+                <button>sepete ekle</button>
+            </div>` ; 
+            x.addEventListener("click", () => {
+                modalac(item);
+            })
+        });   
+    }
+function modalac(item) {
+    document.getElementById("h2").textContent = item.title;
+    document.getElementById("p").textContent = item.description;
+    // document.getElementById("resim2").src = item.image;
+    document.getElementById("mod").style.display = "flex";
+    document.getElementById("kap").onclick = kapatma;
+}
+function kapatma() {
+    document.getElementById("mod").style.display = "none";
+}
